e3d09a284fe23613e39afac2e9b35d9b18384458
Izirae <lautaro.bazzola@gmail.com>
2022-10-24 20:15:09 -0300

    first commit

---
+++ src/utils/colors.js
@@ -0,0 +1,10 @@
+export default {
+  primary: "#EEFFDB",
+  secondary: "#66101F",
+  tertiary: "#855A5C",
+  background: "#F1AB86",
+  text: "#000",
+  white: "#FFFFFF",
+  black: "#000000",
+  gray: "#808080",
+};
